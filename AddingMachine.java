9f5e5beafc85b13d3ed38a8145cd74cdd2164446
yashbhal <yashb1004@gmail.com>
2020-10-06 19:41:29 -0700

    Intial commit

---
+++ AddingMachine.java
@@ -0,0 +1,26 @@
+package cse360assignment02;
+
+public class AddingMachine {        //main class
+    private int total;
+
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    public int getTotal () { //gets the total
+        return 0;
+    }
+
+    public void add (int value) { //add method
+    }
+
+    public void subtract (int value) { //subtract method
+    }
+
+    public String toString () { //toString
+        return "";
+    }
+
+    public void clear() { //clears
+    }
+}
